81f88056576a5b74ae67a8582bfcd16fb573e575
TribeOfBenjamin <benjamin.vasko.33@gmail.com>
2020-06-22 12:03:41 -0500

    added functions for adding departments, roles, and employees

--- server.js
+++ server.js
@@ -3,159 +3,126 @@ const inquirer = require("inquirer");
 
 // create the connection information for the sql database
 let connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "NewYork1991",
-  database: "employees_db"
+    // Your password
+    password: "NewYork1991",
+    database: "employees_db"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    startOptions();
 });
 
 // function which prompts the user for what action they should take
-function start() {
-  inquirer
-    .prompt({
-      name: "startOptions",
-      type: "list",
-      message: "Would you like to do?",
-      choices: ["Add a department", "Add a role", "Add an employee"]
-    })
-    .then(answer => {
-        switch(answer.startOptions) {
-            case "Add a department":
-                //someFunction();
-                break;
-            case "Add a role":
-                //someFunction();
-                break;
-            case "Add an employee":
-                //someFunction();
-                break;
-            default:
-                break;
-        };
-    });
-}
-        
+function startOptions() {
+    inquirer
+        .prompt({
+            name: "startOptions",
+            type: "list",
+            message: "Would you like to do?",
+            choices: ["Add a department", "Add a role", "Add an employee"]
+        })
+        .then(answer => {
+            switch (answer.startOptions) {
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                default:
+                    break;
+            };
+        });
+};
 
-// // function to handle posting new items up for auction
-// function postAuction() {
-//   // prompt for info about the item being put up for auction
-//   inquirer
-//     .prompt([
-//       {
-//         name: "item",
-//         type: "input",
-//         message: "What is the item you would like to submit?"
-//       },
-//       {
-//         name: "category",
-//         type: "input",
-//         message: "What category would you like to place your auction in?"
-//       },
-//       {
-//         name: "startingBid",
-//         type: "input",
-//         message: "What would you like your starting bid to be?",
-//         validate: function(value) {
-//           if (isNaN(value) === false) {
-//             return true;
-//           }
-//           return false;
-//         }
-//       }
-//     ])
-//     .then(function(answer) {
-//       // when finished prompting, insert a new item into the db with that info
-//       connection.query(
-//         "INSERT INTO auctions SET ?",
-//         {
-//           item_name: answer.item,
-//           category: answer.category,
-//           starting_bid: answer.startingBid || 0,
-//           highest_bid: answer.startingBid || 0
-//         },
-//         function(err) {
-//           if (err) throw err;
-//           console.log("Your auction was created successfully!");
-//           // re-prompt the user for if they want to bid or post
-//           start();
-//         }
-//       );
-//     });
-// }
+function addDepartment() {
+    inquirer
+        .prompt({
+            name: "departmentName",
+            type: "input",
+            message: "What is the name of the department you would like to add?"
+        })
+        .then(answer => {
+            connection.query("INSERT INTO department SET name = ?", [answer.departmentName], 
+            function (err) {
+                if (err) throw err;
+                console.log("The " + answer.departmentName + " department was successfully added.");
+                startOptions();
+            }
+            );
+        });
+};
 
-// function bidAuction() {
-//   // query the database for all items being auctioned
-//   connection.query("SELECT * FROM auctions", function(err, results) {
-//     if (err) throw err;
-//     // once you have the items, prompt the user for which they'd like to bid on
-//     inquirer
-//       .prompt([
-//         {
-//           name: "choice",
-//           type: "rawlist",
-//           choices: function() {
-//             let choiceArray = [];
-//             for (let i = 0; i < results.length; i++) {
-//               choiceArray.push(results[i].item_name);
-//             }
-//             return choiceArray;
-//           },
-//           message: "What auction would you like to place a bid in?"
-//         },
-//         {
-//           name: "bid",
-//           type: "input",
-//           message: "How much would you like to bid?"
-//         }
-//       ])
-//       .then(function(answer) {
-//         // get the information of the chosen item
-//         let chosenItem;
-//         for (let i = 0; i < results.length; i++) {
-//           if (results[i].item_name === answer.choice) {
-//             chosenItem = results[i];
-//           }
-//         }
+function addRole() {
+    inquirer
+        .prompt([
+            {
+            name: "roleTitle",
+            type: "input",
+            message: "What is the title of the role you would like to add?"
+            },
+            {
+            name: "roleSalary",
+            type: "input",
+            message: "What is the salary for this role?"
+            }
+        ])
+        .then(answer => {
+            connection.query("INSERT INTO role SET ?", 
+            {
+                title: answer.roleTitle,
+                salary: answer.roleSalary
+            },
+            function (err) {
+                if (err) throw err;
+                console.log("The " + answer.roleTitle + " role with a salary of " + answer.roleSalary + " was successfully added.");
+                startOptions();
+            }
+            );
+        });
+};
 
-//         // determine if bid was high enough
-//         if (chosenItem.highest_bid < parseInt(answer.bid)) {
-//           // bid was high enough, so update db, let the user know, and start over
-//           connection.query(
-//             "UPDATE auctions SET ? WHERE ?",
-//             [
-//               {
-//                 highest_bid: answer.bid
-//               },
-//               {
-//                 id: chosenItem.id
-//               }
-//             ],
-//             function(error) {
-//               if (error) throw err;
-//               console.log("Bid placed successfully!");
-//               start();
-//             }
-//           );
-//         }
-//         else {
-//           // bid wasn't high enough, so apologize and start over
-//           console.log("Your bid was too low. Try again...");
-//           start();
-//         }
-//       });
-//   });
-// }
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+            name: "employeeFirstName",
+            type: "input",
+            message: "What is the employee's first name?"
+            },
+            {
+            name: "employeeLastName",
+            type: "input",
+            message: "What is the employee's last name?"
+            // PROBABLY NEED TO INCLUDE ASSIGN/CONNECT role_id AND manager_id LATER
+            }
+        ])
+        .then(answer => {
+            connection.query("INSERT INTO employee SET ?", 
+            {
+                first_name: answer.employeeFirstName,
+                last_name: answer.employeeLastName
+            },
+            function (err) {
+                if (err) throw err;
+                console.log("The " + answer.addDepartment + " department was successfully added.");
+                startOptions();
+            }
+            );
+        });
+};
+
